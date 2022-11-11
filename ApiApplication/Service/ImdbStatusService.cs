08cf61c08aab8dd5009fdabf22673b1543fc5425
Naeem <n.abbas2021@gmail.com>
2022-11-11 12:01:30 +0500

    Add Service

--- ApiApplication/Service/ImdbStatusService.cs
+++ ApiApplication/Service/ImdbStatusService.cs
@@ -1,33 +1,46 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using ApiApplication.Models;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace ApiApplication.Service
 {
-    public class ImdbStatusService : IHostedService, IDisposable
+    public class ImdbStatusService : IHostedService
     {
         private Timer timer;
         private readonly ILogger<ImdbStatusService> logger;
-
+        public static IMDB_Status imdb_Status = new IMDB_Status();
 
         public ImdbStatusService(ILogger<ImdbStatusService> logger)
         {
             this.logger = logger;
         }
-        public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
-
+     
         public Task StartAsync(CancellationToken cancellationToken)
         {
             timer = new Timer(o => {
                 logger.LogInformation($"IMDB STATUS Api is working");
                 //action
+                var ping = new System.Net.NetworkInformation.Ping();
+
+                var result = ping.Send("www.imdb.com");
+
+                if (result.Status == IPStatus.Success)
+                {
+                    imdb_Status.last_call = DateTime.Now;
+                    imdb_Status.up = true;
+                }
+                else {
+                    imdb_Status.last_call = DateTime.Now;
+                    imdb_Status.up = false;
+                }
+                logger.LogInformation($"IMDB#Up{imdb_Status.up}&Last Call {imdb_Status.last_call}");
+
             },
               null,
               TimeSpan.Zero,
